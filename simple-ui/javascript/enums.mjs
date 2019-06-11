67624fd5a9aba66c3c80553d4fb6ba1357b7241f
James Dean <kreebog@gmail.com>
2019-06-11 14:13:03 -0400

    working on a grid-based simple UI implementation for testing

---
+++ simple-ui/javascript/enums.mjs
@@ -0,0 +1,98 @@
+const TROPHY_IDS = {
+    CHEDDAR_DINNER: 0,
+    DAZED_AND_CONFUSED: 1,
+    DOUBLE_BACKER: 2,
+    FLAWLESS_VICTORY: 3,
+    JUMPING_JACK_FLASH: 4,
+    KICKING_UP_DUST: 5,
+    LIGHT_AT_THE_END: 6,
+    LOOPER: 7,
+    MIGHTY_MOUSE: 8,
+    NERVOUS_WALK: 9,
+    OUT_OF_MOVES: 10,
+    PAPERBACK_WRITER: 11,
+    SCRIBBLER: 12,
+    SHORTCUTTER: 13,
+    SPINNING_YOUR_WHEELS: 14,
+    TAKING_A_STAND: 15,
+    STANDING_AROUND: 16,
+    THE_LONGER_WAY_HOME: 17,
+    THE_LONGEST_WAY_HOME: 18,
+    THE_LONG_WAY_HOME: 19,
+    TOO_HOT_TO_HANDLE: 20,
+    WASTED_TIME: 21,
+    WATCHING_PAINT_DRY: 22,
+    WISHFUL_DYING: 23,
+    WISHFUL_THINKING: 24,
+    YOU_FELL_FOR_IT: 25,
+    YOU_FOUGHT_THE_WALL: 26
+};
+const PLAYER_STATES = {
+    NONE: 0,
+    STANDING: 1,
+    SITTING: 2,
+    LYING: 4,
+    STUNNED: 8,
+    BLIND: 16,
+    BURNING: 32,
+    LAMED: 64,
+    BEARTRAPPED: 128,
+    SLOWED: 256,
+    DEAD: 512
+};
+const DIRS = {
+    NONE: 0,
+    NORTH: 1,
+    SOUTH: 2,
+    EAST: 4,
+    WEST: 8
+};
+const CELL_TAGS = {
+    NONE: 0,
+    START: 1,
+    FINISH: 2,
+    PATH: 4,
+    CARVED: 8,
+    LAVA: 16
+};
+const CELL_TRAPS = {
+    NONE: 0,
+    PIT: 1,
+    BEARTRAP: 2,
+    TARPIT: 4,
+    FLAMETHOWER: 8
+};
+const COMMANDS = {
+    NONE: 0,
+    LOOK: 1,
+    SIT: 2,
+    STAND: 3,
+    MOVE: 4,
+    JUMP: 5,
+    WRITE: 6,
+    QUIT: 7
+};
+const GAME_RESULTS = {
+    NONE: 0,
+    IN_PROGRESS: 1,
+    OUT_OF_MOVES: 2,
+    OUT_OF_TIME: 3,
+    DEATH_TRAP: 4,
+    DEATH_POISON: 5,
+    DEATH_LAVA: 6,
+    WIN: 7,
+    WIN_FLAWLESS: 8,
+    ABANDONED: 9
+};
+const GAME_STATES = {
+    NEW: 0,
+    IN_PROGRESS: 1,
+    FINISHED: 2,
+    ABORTED: 3,
+    ERROR: 4
+};
+const GAME_MODES = {
+    NONE: 0,
+    SINGLE_PLAYER: 1,
+    MULTI_PLAYER: 2
+};
---
+++ simple-ui/javascript/simple-ui.js
@@ -0,0 +1,153 @@
+let teams;
+let curMaze;
+const curLoc = {row: 0, col: 0};
+const prevLoc = {row: 0, col: 0};
+
+function movePlayer(newRow, newCol, dir) {
+    // store previous location
+    prevLoc.row = curLoc.row;
+    prevLoc.col = curLoc.col;
+
+    // set new location
+    curLoc.row = newRow;
+    curLoc.col = newCol;
+
+    // grab the right cell elements
+    const prevLocDiv = $('#' + prevLoc.row + '-' + prevLoc.col);
+    const curLocDiv = $('#' + curLoc.row + '-' + curLoc.col);
+
+    const prevCell = curMaze.cells[prevLoc.row][prevLoc.col];
+    const curCell = curMaze.cells[curLoc.row][curLoc.col];
+
+    // clean up the previous (now empty) cell
+    styleCellDiv(prevCell, prevLocDiv, curLoc);
+
+    // update the newly, rat-filled cell
+    styleCellDiv(curCell, curLocDiv, curLoc);
+
+    // remark the path if player stepped on it
+    if (!!(curCell.tags & CELL_TAGS.PATH) || !!(prevCell.tags & CELL_TAGS.PATH)) {
+        console.log('Player marred the path, redrawing..');
+        markPath(curMaze, curMaze.cells[curMaze.startCell.row][curMaze.startCell.col], DIRS.NONE);
+    }
+}
+
+function startBotGame(teamId, botId, mazeId) {}
+function doAction(command, direction, message) {}
+
+function loadTeamSelect() {
+    console.log('Loading teams list...');
+    $.getJSON('http://mazemasterjs.com/api/team/get', (data) => {
+        teams = data;
+        for (const team of teams) {
+            let opt = "<option value='" + team.id + "'>";
+            opt += team.name;
+            opt += '</option>';
+            $('#selTeam').append(opt);
+        }
+        loadBotSelect(teams[0].id);
+    });
+}
+
+function loadBotSelect(teamId) {
+    console.log('Loading bots for team ' + teamId);
+    if (!teams || !teamId) return;
+
+    $('#selBot').empty();
+    const team = teams.find((team) => {
+        return team.id === teamId;
+    });
+
+    for (const bot of team.bots) {
+        let botSel = "<option val='" + bot.id + "'>";
+        botSel += bot.name + ' (' + bot.coder + ')';
+        botSel += '</option>';
+        $('#selBot').append(botSel);
+    }
+}
+
+function loadGameSelect() {
+    console.log('Loading active games list...');
+    $.getJSON('http://mazemasterjs.com/game/get', (games) => {
+        console.log(games);
+        return;
+        for (const game of games) {
+            let opt = "<option value='" + game.id + "'>";
+            opt += game.team.name + ' : ' + game.maze.name;
+            opt += '</option>';
+            $('#selGame').append(opt);
+        }
+    });
+}
+
+/**
+ * Load all maze stub data from the maze-service
+ *
+ * @param {*} mazeId
+ */
+function loadMazeSelect() {
+    console.log('Loading maze list...');
+    $.getJSON('http://mazemasterjs.com/api/maze/get', (mazes) => {
+        for (const maze of mazes) {
+            let opt = "<option value='" + maze.id + "'>";
+            opt += maze.name + ' (' + maze.height + ' x ' + maze.width + ')';
+            opt += '</option>';
+            $('#selMaze').append(opt);
+        }
+    });
+}
+
+/**
+ * Load the requested maze from the maze-service
+ *
+ * @param {*} mazeId
+ */
+function loadMaze(mazeId) {
+    console.log('Loading maze ' + mazeId);
+    $('#loadingScreen').toggle();
+
+    $.getJSON('http://mazemasterjs.com/api/maze/get?id=' + mazeId, (data) => {
+        // set the maze global
+        curMaze = data[0];
+
+        $(':root').css('--rows', curMaze.cells.length);
+        $(':root').css('--cols', curMaze.cells[0].length);
+
+        // set player location to start cell
+        curLoc.row = curMaze.startCell.row;
+        curLoc.col = curMaze.startCell.col;
+
+        // $('#textRender').text(curMaze.textRender);
+
+        // render the maze
+        renderMaze(curMaze);
+
+        // hide the loading screen
+        $('#loadingScreen').toggle();
+    });
+}
+
+/**
+ * Returns the opposite direction
+ *
+ * @param {*} dir
+ */
+function reverseDir(dir) {
+    switch (dir) {
+        case DIRS.NORTH: {
+            return DIRS.SOUTH;
+        }
+        case DIRS.SOUTH: {
+            return DIRS.NORTH;
+        }
+        case DIRS.EAST: {
+            return DIRS.WEST;
+        }
+        case DIRS.WEST: {
+            return DIRS.EAST;
+        }
+        default: {
+            return DIRS.NONE;
+        }
+    }
+}
