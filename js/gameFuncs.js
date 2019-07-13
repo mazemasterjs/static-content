1a1f227d8eea9ddd7b24d081c09f5698c9ecd614
James Dean <kreebog@gmail.com>
2019-07-13 15:17:03 -0400

    fixed game server url

--- js/gameFuncs.js
+++ js/gameFuncs.js
@@ -3,8 +3,8 @@
 /* eslint-disable no-unused-vars */
 const MAZE_URL = 'http://mazemasterjs.com/api/maze';
 // const GAME_URL = 'http://game-server-maze-master-js.b9ad.pro-us-east-1.openshiftapps.com/game';
-// const GAME_URL = 'http://mazemasterjs.com/game';
-const GAME_URL = 'http://localhost:8080/game';
+const GAME_URL = 'http://mazemasterjs.com/game';
+// const GAME_URL = 'http://localhost:8080/game';
 const TEAM_URL = 'http://mazemasterjs.com/api/team';
 
 // global configuration vars
